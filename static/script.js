22040837020e0a5d86ea4821267566c6b9564121
DXU <danxu0910@gmail.com>
2020-07-09 18:42:42 +0200

    add function for fetching 3 values and getting new about

--- static/script.js
+++ static/script.js
@@ -1,15 +1,10 @@
 window.onload = () => {
-    const title_slider = document.querySelector("#title_slider");
-    const people_slider = document.querySelector("#people_slider");
-    const project_slider = document.querySelector("#project_slider");
 
-    const button = document.querySelector("#button1");
+    const generate = () => {
+        const title_slider = document.querySelector("#title_slider");
+        const people_slider = document.querySelector("#people_slider");
+        const project_slider = document.querySelector("#project_slider");
 
-    const title = document.querySelector("#title");
-    const people = document.querySelector("#people");
-    const project = document.querySelector("#project");
-
-    button.onclick = () => {
         const formdata = new FormData();
         formdata.append("title_slider", title_slider.value);
         formdata.append("people_slider", people_slider.value);
@@ -21,12 +16,49 @@ window.onload = () => {
         }).then((res) => {
             return res.json();
         }).then((data) => {
-            title.innerHTML = data["new_title"];
-            people.innerHTML = data["new_people"];
+            const title = document.querySelector("#title");
+            const people = document.querySelector("#people");
+            const project = document.querySelector("#project");
+
+            title.innerHTML = "&nbsp;// " + data["new_title"] + " //&nbsp;";
+            people.innerHTML = "By " + data["new_people"];
             project.innerHTML = data["new_project"];
 
             console.log(data);
         });
-        // console.log(title_slider.value);
     };
+
+    const art_value = document.querySelector("#art_value");
+    const sci_value = document.querySelector("#sci_value");
+    const tech_value = document.querySelector("#tech_value");
+
+    const about = () => {
+        const formdata = new FormData();
+        formdata.append("art_value", art_value.value);
+        formdata.append("sci_value", sci_value.value);
+        formdata.append("tech_value", tech_value.value);
+
+        fetch("/about", {
+            method: "POST",
+            body: formdata
+        }).then((res) => {
+            return res.text();
+        }).then((data) => {
+            const newabout = document.querySelector("#newabout");
+
+            newabout.innerHTML = data;
+
+            console.log(data);
+        });
+    };
+
+    const button = document.querySelector("#button1");
+
+    button.onclick = generate;
+    generate();
+
+    art_value.oninput = about;
+    sci_value.oninput = about;
+    tech_value.oninput = about;
+    about();
 };
