3801097d25d32f49391509de779571d7b9bd5e79
DXU <danxu0910@gmail.com>
2020-07-09 11:50:54 +0200

    add route for generate new projects

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+import markov
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "hello ancient puppy"
+    return render_template("index.html")
+
+
+@app.route("/generate")
+def generate():
+    new_title = markov.markov_generate_from_lines_in_file(2, "./nmnt/nmnt-title.txt", 1, "char", max_gen=200)
+    new_people = markov.markov_generate_from_lines_in_file(2, "./nmnt/nmnt-people.txt", 1, "char", max_gen=200)
+    new_project = markov.markov_generate_from_lines_in_file(2, "./nmnt/nmnt-content.txt", 1, "word", max_gen=1000)
+    
+    return jsonify({
+        "new_title" : new_title[0],
+        "new_people" : new_people[0],
+        "new_project" : new_project[0]
+    })
 
 
 if __name__ == "__main__":
